859c2b62bd9d6048cb0c5b18b64098ac821eadcf
Sofia Estrela Bernardes <130997583+sofhkkj@users.noreply.github.com>
2024-10-03 19:54:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,31 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teste Prova</title>
 </head>
-<body style="text-align: center; background-color: lightblue; ">
+<body style="text-align: center; background-color: lightblue;">
     <h2>Cadastro Currículo</h2>
     <form action="" method="POST">
         <label for="nome">Nome:</label><br>
-        <input type="text" id="nome" name="nome" required><br><br>
+        <input type="text" name="nome" required><br><br>
 
         <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email" required><br><br>
+        <input type="email" name="email" required><br><br>
 
         <label for="telefone">Telefone:</label><br>
-        <input type="tel" id="telefone" name="telefone"><br><br>
+        <input type="tel" name="telefone"><br><br>
 
         <label for="nascimento">Data de Nascimento:</label><br>
-        <input type="date" id="nascimento" name="data" required><br><br>
+        <input type="date" name="data" required><br><br>
 
-        <label for="genero">Gênero:</label><br>
-        <input type="radio" id="masculino" name="genero" value="masculino">
-        <label for="masculino">Masculino</label><br>
-        <input type="radio" id="feminino" name="genero" value="feminino">
-        <label for="feminino">Feminino</label><br>
-        <input type="radio" id="outro" name="genero" value="outro">
-        <label for="outro">Outro</label><br><br>
+        <label>Gênero:</label><br>
+        <input type="radio" name="genero" value="masculino">
+        <label>Masculino</label><br>
+        <input type="radio" name="genero" value="feminino">
+        <label>Feminino</label><br>
+        <input type="radio" name="genero" value="outro">
+        <label>Outro</label><br><br>
 
         <label for="escolaridade">Escolaridade:</label><br>
-        <select id="escolaridade" name="escolaridade" required>
+        <select name="escolaridade" required>
             <option value="">Selecione</option>
             <option value="fundamental">Ensino Fundamental</option>
             <option value="medio">Ensino Médio</option>
@@ -37,8 +37,11 @@
             <option value="pos-graduacao">Pós-graduação</option>
         </select><br><br>
 
-        <label for="mensagem">Conte sobre você:</label><br>
-        <textarea id="mensagem" name="mensagem" rows="4" cols="20"></textarea><br><br>
+        <label for="mensagem">Conte um pouco sobre você:</label><br>
+        <textarea name="mensagem" rows="4" cols="20"></textarea><br><br>
+ 
+        <label for="mensagem">Experiências profissionais:</label><br>
+        <textarea name="mensagem" rows="4" cols="20"></textarea><br><br>
 
         <input type="submit" value="Enviar">
     </form>
@@ -52,6 +55,7 @@
         $genero = $_POST['genero'];
         $escolaridade = $_POST['escolaridade'];
         $mensagem = $_POST['mensagem'];
+        $mensagem = $_POST['mensagem'];
 
         print("<h1>Currículo do(a): $nome</h1>");
         print("<p>Email: $email</p>");
@@ -59,8 +63,10 @@
         print("<p>Data de nascimento: $data</p>");
         print("<p>Gênero: $genero</p>");
         print("<p>Escolaridade: $escolaridade</p>");
-        print("<p>Mensagem: $mensagem</p>");
+        print("<p>Perfil Pessoal: $mensagem</p>");
+        print("<p> Experiências profissionais: $mensagem</p>");
     }
+
     ?>
 </body>
 </html>
